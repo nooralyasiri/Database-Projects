ee248c0f78c72f62475f7b9fe1be569381840c29
Eduardo Martinez <e.martinez.em47@gmail.com>
2022-02-11 13:24:54 -0600

    start

---
+++ SQL_Queries.sql
@@ -0,0 +1,25 @@
+/*Train table*/
+Create Table Train
+  (T_number       INT         NOTNULL,
+   T_name         VARCHAR(15) NOTNULL,
+   T_source       VARCHAR(15) NOTNULL,
+   T_destination  VARCHAR(15) NOTNULL,
+   T_P_tickets    INT
+   T_G_tickets    INT
+   T_weekdays     VARCHAR(15)
+   PRIMARY KEY(),
+   UNIQUE(),
+   FOREIGN KEY(), REFERENCES());
+  )
+
+/*Passenger table*/
+Create Table Passenger
+  (P_name      VARCHAR(15) NOTNULL,
+   P_age       INT         NOTNULL,
+   P_address   VARCHAR(15) NOTNULL,
+   P_status    VARCHAR(1)  NOTNULL,
+   P_catergory VARCHAR(1)  NOTNULL,
+   PRIMARY KEY(),
+   UNIQUE(),
+   FOREIGN KEY(), REFERENCES());
+  )
