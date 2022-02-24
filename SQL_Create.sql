0c7809b78b8656c6ab6e29461de0a50ece44d4ee
nicolen21 <93563036+nicolen21@users.noreply.github.com>
2022-02-23 20:23:04 -0600

    Update SQL_Create.sql

--- SQL_Create.sql
+++ SQL_Create.sql
@@ -1,3 +1,5 @@
+/* CREATING TABLES */
+
 /* TRAIN TABLE */
 CREATE TABLE Train
   (
@@ -11,16 +13,6 @@ CREATE TABLE Train
    PRIMARY KEY([TrainName])
   );
 
-/* TRAIN DATA */
-  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(1,"Orient Express","Paris","Istanbul",800,600,"Weekdays");
-  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(2,"Flying Scotsman","Edinburgh","London",4000,3500,"Friday,Saturday,Sunday");
-  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(3,"Golden Arrow","Victoria","Dover",980,860,"Monday,Tuesday,Wednesday");
-  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(4,"Golden Chariot","Bangalore","Goa",4300,3800,"Saturday,Sunday");
-  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(5,"Maharaja Express","Delhi","Mumbai",5980,4510,"Wednesday,Thursday,Friday");
-
-
-/* ========================================================================================= */
-
 
 /* PASSENGER TABLE */
 CREATE TABLE Passenger
@@ -31,31 +23,6 @@ CREATE TABLE Passenger
    [PassengerSSN]       INTEGER(9)    NOT NULL,
    PRIMARY KEY([PassengerSSN])
  );
-
-/* PASSENGER DATA */
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("James Butt",53,"6649 N Blue Gum St, New Orleans",264816896);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Josephine Darakjy",46,"4 B Blue Ridge Blvd, Brighton",240471168);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Art Venere",39,"8 W Cerritos Ave #54, Bridgeport",285200976);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Lenna	Paprocki",43,"639 Main St, Anchorage",309323096);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Donette	Foller",31,"34 Center St, Hamilton",272610795);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Simona Morasca",27,"3 Mcauley Dr, Ashland",250951162);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Mitsue Tollner",37,"7 Eads St, Chicago",272913578);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Leota	Dilliard",30,"7 W Jackson Blvd, San Jose",268682534);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Sage	Wieser",39,"5 Boston Ave #88, Sioux Falls",310908858);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Kris	Marrier",65,"228 Runamuck Pl #2808,Baltimore",322273872);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Minna	Amigon",26,"2371 Jerrold Ave, Kulpsville",256558303);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Abel	Maclead",61,"37275 St  Rt 17m M, Middle Island",302548590);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Kiley	Caldarera",40,"25 E 75th St #69, Los Angeles",284965676);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Graciela	Ruta",41,"98 Connecticut Ave Nw, Chagrin Falls",277292710);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Cammy	Albares",65,"56 E Morehead St, Laredo",331160133);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Mattie	Poquette",26,"73 State Road 434 E, Phoenix",331293204);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Meaghan	Garufi",61,"69734 E Carrillo St, Mc Minnville",290123298);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Gladys	Rim",30,"322 New Horizon Blvd, Milwaukee",286411536);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Yuki	Whobrey",36,"1 State Route 27, Taylor",294860856);
-  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Fletcher	Flosi",60,"394 Manchester Blvd, Rockford",317434088);
-
-
-/* ========================================================================================= */
 
 
 /* TRAIN STATUS TABLE */
@@ -70,17 +37,7 @@ CREATE TABLE TrainStatus
    FOREIGN KEY([TrainName]) REFERENCES [Train] ([TrainName])
    ON DELETE NO ACTION ON UPDATE NO ACTION
  );
-
 CREATE INDEX [IFK_StatusOfTrain] ON [Train] ([TrainName]);
-
-/* TRAIN STATUS DATA */
-INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(19/02/2022,"Orient Express",10,10,0,0);
-INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(20/02/2022,"Flying Scottsman",8,5,2,5);
-INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(21/02/2022,"Guwahati Express",7,6,3,4);
-INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(21/02/2022,"Golden Chariot",6,3,4,7);
-
-
-/* ========================================================================================= */
 
 
 /* TICKET TABLE */
@@ -95,10 +52,60 @@ CREATE TABLE Ticket
     FOREIGN KEY([PassengerSSN]) REFERENCES [Passenger] ([PassengerSSN])
     ON DELETE NO ACTION ON UPDATE NO ACTION
   );
+  
+/* ============================================================================================================================ */
+/* CREATING FOREIGN KEY INDEXES */
 
+CREATE INDEX [IFK_StatusOfTrain] ON [Train] ([TrainName]);
 CREATE INDEX [IFK_TrainTickets] ON [Train] ([TrainNumber]);
 CREATE INDEX [IFK_PassengerTicket] ON [Passenger] ([PassengerSSN]);
 
+
+/* ============================================================================================================================ */
+/* POPULATING TABLES */
+
+
+/* PASSENGER DATA */
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("James Butt",53,"6649 N Blue Gum St, New Orleans",264816896);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Josephine Darakjy",46,"4 B Blue Ridge Blvd, Brighton",240471168);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Art Venere",39,"8 W Cerritos Ave #54, Bridgeport",285200976);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Lenna Paprocki",43,"639 Main St, Anchorage",309323096);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Donette Foller",31,"34 Center St, Hamilton",272610795);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Simona Morasca",27,"3 Mcauley Dr, Ashland",250951162);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Mitsue Tollner",37,"7 Eads St, Chicago",272913578);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Leota Dilliard",30,"7 W Jackson Blvd, San Jose",268682534);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Sage Wieser",39,"5 Boston Ave #88, Sioux Falls",310908858);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Kris Marrier",65,"228 Runamuck Pl #2808,Baltimore",322273872);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Minna Amigon",26,"2371 Jerrold Ave, Kulpsville",256558303);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Abel Maclead",61,"37275 St  Rt 17m M, Middle Island",302548590);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Kiley Caldarera",40,"25 E 75th St #69, Los Angeles",284965676);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Graciela Ruta",41,"98 Connecticut Ave Nw, Chagrin Falls",277292710);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Cammy Albares",65,"56 E Morehead St, Laredo",331160133);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Mattie Poquette",26,"73 State Road 434 E, Phoenix",331293204);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Meaghan Garufi",61,"69734 E Carrillo St, Mc Minnville",290123298);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Gladys Rim",30,"322 New Horizon Blvd, Milwaukee",286411536);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Yuki Whobrey",36,"1 State Route 27, Taylor",294860856);
+  INSERT INTO [Passenger]([PassengerName],[PassengerAge],[PassengerAddress],[PassengerSSN]) VALUES("Fletcher Flosi",60,"394 Manchester Blvd, Rockford",317434088);
+  
+  
+
+/* TRAIN DATA */
+  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(1,"Orient Express","Paris","Istanbul",800,600,"Weekdays");
+  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(2,"Flying Scotsman","Edinburgh","London",4000,3500,"Friday,Saturday,Sunday");
+  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(3,"Golden Arrow","Victoria","Dover",980,860,"Monday,Tuesday,Wednesday");
+  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(4,"Golden Chariot","Bangalore","Goa",4300,3800,"Saturday,Sunday");
+  INSERT INTO [Train]([TrainNumber],[TrainName],[TrainSource],[TrainDestination],[PremiumFare],[GeneralFare],[Schedule]) VALUES(5,"Maharaja Express","Delhi","Mumbai",5980,4510,"Wednesday,Thursday,Friday");
+  
+  
+  
+/* TRAIN STATUS DATA */
+  INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(19/02/2022,"Orient Express",10,10,0,0);
+  INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(20/02/2022,"Flying Scottsman",8,5,2,5);
+  INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(21/02/2022,"Guwahati Express",7,6,3,4);
+  INSERT INTO [TrainStatus]([TrainDate],[TrainName],[PremSeatsOpen],[GenSeatsOpen],[PremSeatsOccupied],[GenSeatsOccupied]) VALUES(21/02/2022,"Golden Chariot",6,3,4,7);
+  
+  
+  
 /* TICKET DATA */
    INSERT INTO [Ticket]([PassengerSSN],[TrainNumber],[TicketType],[TicketStatus]) VALUES(264816896,3,"Premium","Booked");
    INSERT INTO [Ticket]([PassengerSSN],[TrainNumber],[TicketType],[TicketStatus]) VALUES(240471168,2,"General","Booked");
