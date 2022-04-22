4520188517c167ff775bf8ee523d89ac8c9aef6f
Noor <60522271+nooralyasiri@users.noreply.github.com>
2022-04-22 14:02:02 -0500

    Update Car_Rental_Queries_New.sql

--- Car_Rental_Queries_New.sql
+++ Car_Rental_Queries_New.sql
@@ -49,3 +49,39 @@ WHERE
   ORDER BY StartDate ASC, R.StartDate;
  
  ----Needs editing----
+ 
+ CREATE VIEW vRentalInfo
+AS
+SELECT 
+  OrderDate, 
+  StartDate, -- in an ascending order  
+  ReturnDate, 
+  Total Days per Rental as TotalDays, -- you need to change weeks to days  
+  VehicleID as VIN, 
+  Description as Vehicle,
+
+  Type, --you need to use the description of the type  
+  CASE
+		WHEN Type = '1' THEN 'Compact'
+		WHEN Type = '2' THEN 'Medium'
+		WHEN Type = '3' THEN 'Large'
+		WHEN Type = '4' THEN 'SUV'
+		WHEN Type = '5' THEN 'Truck'
+		WHEN Type = '6' THEN 'VAN'
+	END AS Type,
+ 
+  CASE
+    WHEN Category = '0' THEN 'Basic'
+    WHEN Category = '1' THEN 'Luxury'
+  END AS Category,
+
+  CustID AS CustomerID,  
+  Name as CustomerName,  
+  TotalAmount as OrderAmount, 
+  TotalAmount as RentalBalance
+FROM
+  RENTAL
+WHERE
+  V.Type = T.Type
+  AND V.Category = T.Category
+  ORDER BY StartDate ASC, R.StartDate;
