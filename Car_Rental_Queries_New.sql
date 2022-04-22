5c56a67dc787cd136499a7b6a7fda3a44d841810
nicolen21 <93563036+nicolen21@users.noreply.github.com>
2022-04-22 15:00:19 -0500

    Update Car_Rental_Queries_New.sql

--- Car_Rental_Queries_New.sql
+++ Car_Rental_Queries_New.sql
@@ -10,47 +10,40 @@ UPDATE RENTAL
 SET Returned = 0
 WHERE PaymentDate IS NULL;
 
----- Needs testing ----
-
---Q2
+-- Q2
 CREATE VIEW vRentalInfo
 AS
 SELECT 
-  OrderDate, 
-  StartDate, -- in an ascending order  
-  ReturnDate, 
-  --Total Days per Rental as TotalDays, -- you need to change weeks to days 
-  JULIANDAY(R.returnDate) - JULIANDAY(R.startDate) AS 'TotalDays', 
-  V.VehicleID as VIN, 
-  V.Description as Vehicle,
-  --you need to use the description of the type  
-  CASE
-	WHEN V.Type = '1' THEN 'Compact'
-	WHEN V.Type = '2' THEN 'Medium'
-	WHEN V.Type = '3' THEN 'Large'
-	WHEN V.Type = '4' THEN 'SUV'
-	WHEN V.Type = '5' THEN 'Truck'
-	WHEN V.Type = '6' THEN 'VAN'
-  END AS Type,
-  --you need to use the description of the category  
-  CASE
-    WHEN V.Category = '0' THEN 'Basic'
-    WHEN V.Category = '1' THEN 'Luxury'
-  END AS Category,
-  C.CustID AS CustomerID,  
-  C.Name as CustomerName,  
-  TotalAmount as OrderAmount, 
-  (
-    SELECT SUM(TotalAmount) AS 'RentalBalance'
-    FROM Rental as R
-    WHERE PaymentDate IS NULL;
-  )
-FROM
-  RENTAL as R, VEHICLE AS V, RATE AS T, CUSTOMER AS C
-WHERE
-  V.Type = T.Type
-  AND V.Category = T.Category
-  AND C.CustID = V.CustID
-  ORDER BY StartDate ASC;
- 
- ----Needs Testing----
+	OrderDate, 
+	StartDate,
+	ReturnDate, 
+	JULIANDAY(R.returnDate) - JULIANDAY(R.startDate) AS 'TotalDays', 
+	V.VehicleID as VIN, 
+	Description as Vehicle, 
+	CASE
+		WHEN V.Type = '1' THEN 'Compact'
+		WHEN V.Type = '2' THEN 'Medium'
+		WHEN V.Type = '3' THEN 'Large'
+		WHEN V.Type = '4' THEN 'SUV'
+		WHEN V.Type = '5' THEN 'Truck'
+		WHEN V.Type = '6' THEN 'VAN'
+	END AS Type,
+	CASE
+		WHEN V.Category = '0' THEN 'Basic'
+		WHEN V.Category = '1' THEN 'Luxury'
+	END AS Category,
+	C.CustID AS CustomerID,  
+	C.Name as CustomerName,  
+	TotalAmount as OrderAmount, 
+	CASE
+		WHEN Returned = '1' THEN '0'
+		WHEN Returned = '0' THEN TotalAmount
+		END AS RentalBalance
+	FROM
+	CUSTOMER as C, RENTAL as R, VEHICLE AS V, RATE AS T
+	WHERE
+	V.Type = T.Type
+	AND V.Category = T.Category
+	AND C.custID = R.custID
+	AND R.VehicleID = V.VehicleID
+	ORDER BY StartDate ASC;
